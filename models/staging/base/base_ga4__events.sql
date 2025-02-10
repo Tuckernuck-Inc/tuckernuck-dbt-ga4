2bb967be19dcccc35adba2b8e2686c4cafdf1f80
the-data-driven-co <98104600+the-data-driven-co@users.noreply.github.com>
2025-02-10 12:41:27 -0800

    update base_ga4_events incremental

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -20,8 +20,10 @@
 with 
 source as (
     select _table_suffix 
-    from {{ source('ga4', 'events') }})
-    where (_table_suffix not like '%\\_%' or _table_suffix like '%fresh\\_%'
+    from {{ source('ga4', 'events') }}
+    where 
+        (_table_suffix not like '%\\_%' or _table_suffix like '%fresh\\_%')
+        and cast(right(_table_suffix, 8) as int64) >= {{var('start_date')}}
     {% if is_incremental() %}
         -- this filter will only be applied on an incremental run
         and parse_date('%Y%m%d', right(_table_suffix, 8)) in ({{ partitions_to_replace | join(',') }})
@@ -33,7 +35,6 @@ standard_partitions as (
     from source
     where 
         _table_suffix not like '%\\_%'
-        and cast(right(_table_suffix, 8) as int64) >= {{var('start_date')}}
 ),
 
 fresh_partitions as (
@@ -54,9 +55,15 @@ source_deduped as (
     select
         {{ ga4.base_select_source() }}
     from {{ source('ga4', 'events') }}
-    where _table_suffix in (
-        select _table_suffix from clean_table_suffix_list
-    )
+    where  
+        (_table_suffix not like '%\\_%' or _table_suffix like '%fresh\\_%')
+        and cast(right(_table_suffix, 8) as int64) >= {{var('start_date')}}
+
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        and parse_date('%Y%m%d', right(_table_suffix, 8)) in ({{ partitions_to_replace | join(',') }})
+    {% endif %}
+
 ),
 
 renamed as (
